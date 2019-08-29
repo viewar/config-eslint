4a2b5a8345db35049a7b168381d0725a760db7ee
DoubleU23 <dev@project23.org>
2019-08-30 00:17:39 +0200

    add some testcode

---
+++ __tests__/testcode.js
@@ -0,0 +1,17 @@
+import { failedImportNamed } from '../env/node';
+
+const failedImport  = require('../env/noddfsfe.js');
+const failedImport2 = require('../env/node.js');
+
+const test = (test) => {
+  if (var1 == 'test') {
+    const test1 = true;
+    const test2 = false;
+  }
+  console.log('failedImportNamed :', failedImportNamed);
+};
+
+const testObject = {
+  test: 'fdsfg',
+  foo:  'bar',
+};
