d5487577e470fe25ea6c5e6d0775b348676d0c8b
SF <sf@viewar.com>
2019-08-30 13:35:55 +0200

    lint: enhance ruleset
    * array/object bracket styles
    * config-standard overwrites (will be added in future release)
    * prevent key-spacing for onle-liners and set `{mode: 'minimum'}`
    *

--- env/node.js
+++ env/node.js
@@ -22,22 +22,20 @@ module.exports = {
       flowVersion: '0.53', // Flow version
     },
     // TODO: clearify (and refactor?) resolver settings
-    'import/resolver': {
-      node: {
-        // paths:      ['.'],
-        // extensions: [ '.js', '.jsx' ],
-      },
-      webpack: {
-        paths:      [ 'src', '__tests__' ],
-        extensions: [ '.js', '.jsx' ],
-      },
-    },
-    'import/cache': {
-      lifetime: 5,
-    },
+    // 'import/resolver': {
+    //   node: {
+    //     paths:      [ '.' ],
+    //     extensions: [ '.js' ],
+    //   },
+    //   webpack: {
+    //     paths:      [ 'src', '__tests__' ],
+    //     extensions: [ '.js', '.jsx' ],
+    //   },
+    // },
+    'import/cache': { lifetime: 5 },
   },
-  plugins: [ 'node', 'react', 'import', 'promise' ],
-  extends: [
+  plugins:   [ 'node', 'react', 'import', 'promise' ],
+  'extends': [
     'eslint:recommended',
     'plugin:node/recommended',
     'plugin:import/warnings',
@@ -48,14 +46,26 @@ module.exports = {
 
   rules: {
     // STANDARD OVERWRITES/ADDITIONS
-    camelcase: [
-      'error', {
-        ignoreDestructuring: true,
-        properties:          'never', // ignores properites case
-      },
-    ],
+    camelcase: [ 'error', {
+      ignoreDestructuring: true,
+      properties:          'never', // ignores properites case
+    }],
     'block-spacing': [ 'error', 'always' ],
     yoda:            [ 'error', 'never' ],
+
+    'no-undefined':   'error', // use "null" instead of undefined
+    'no-undef':       [ 'error' ],
+    'no-unused-vars': [ 'warn', {
+      args:               'after-used',
+      vars:               'local',
+      ignoreRestSiblings: true,
+      caughtErrors:       'all',
+    }],
+
+    'no-unused-expressions': [ 'error', {
+      allowShortCircuit: true,
+      allowTernary:      true,
+    }],
 
     /**
      * PRETTIER equivalents
@@ -63,148 +73,129 @@ module.exports = {
      * otherwise you'll see eslint re-formating prettier changes
      */
     // printWidth
-    'max-len': [
-      'warn', 120, {
-        ignoreComments: true,
+    'max-len':  [ 'warn', 120, { ignoreComments: true }],
+    indent:    [ 'error', 2, {
+      VariableDeclarator: {
+        'var':   2,
+        let:     2,
+        'const': 1,
       },
-    ],
-    indent: [
-      'error', 2, {
-        VariableDeclarator: {
-          var:   2,
-          let:   2,
-          const: 1,
-        },
-        ignoreComments: false,
-      },
-    ],
-    semi:            [ 'error', 'always' ], // semi
-    quotes:          [ 'error', 'single' ], // singleQuote
-    'comma-spacing': [
-      'error', {
-        before: false,
-        after:  true,
-      },
-    ],
-    'comma-dangle':                       [ 'error', 'always-multiline' ], // trailingComma
-    'arrow-parens':                       [ 'error', 'always' ],
+      ignoreComments: false,
+    }],
+    semi:                                 [ 'error', 'always' ], // prettier.semi
+    quotes:                               [ 'error', 'single' ], // prettier.singleQuote
+    'comma-spacing':                      [ 'error', {
+      before: false,
+      after:  true,
+    }],
+    'comma-dangle':                       [ 'error', 'always-multiline' ], // prettier.trailingComma
+    'arrow-parens':                       [ 'error', 'always' ], // prettier.arrowParens
     'react/jsx-closing-bracket-location': [ 1, 'line-aligned' ],
 
     // format
-    'quote-props': [
-      'error', 'as-needed', {
-        keywords:    false,
-        unnecessary: true,
+
+    /**
+     * OBJECTS
+     */
+    'quote-props':          [ 'error', 'as-needed', {
+      keywords:    true,
+      unnecessary: true,
+    }],
+    'object-curly-newline':    [ 'error', {
+      ObjectExpression:  {
+        multiline:     true,
+        consistent:    true,
+        minProperties: 2,
       },
-    ],
-    'object-property-newline': [
-      'error', {
-        allowMultiplePropertiesPerLine: false,
+      ObjectPattern:     {
+        multiline:     true,
+        consistent:    true,
+        minProperties: 2,
       },
-    ],
-    'object-curly-newline': [
-      'error', {
-        ObjectPattern:     { multiline: true },
-        ImportDeclaration: 'never',
-        ExportDeclaration: {
-          multiline: true,
-        },
+      ImportDeclaration: 'never',
+      ExportDeclaration: {
+        multiline:     true,
+        minProperties: 2,
       },
-    ],
-    'object-curly-spacing': [ 'error', 'always', {
+    }],
+    'object-property-newline': [ 'error', {
+      allowMultiplePropertiesPerLine: false,
+    }],
+    'object-curly-spacing':    [ 'error', 'always', {
       objectsInObjects: false,
       arraysInObjects:  false,
     }],
-    // 'array-element-newline': [ 'error', {
-    //   multiline: false,
-    // }],
-    // 'array-bracket-newline':   [
-    //   'error',
-    //   {
-    //     multiline: true,
-    //     minItems:  5
-    //   }
-    // ],
+    /**
+     * ARRAYS
+     */
+    'array-element-newline': [ 'error', 'consistent' ],
+    'array-bracket-newline':   [ 'error', 'consistent' ],
     'array-bracket-spacing': [ 'error', 'always', {
-      singleValue:     false,
+      singleValue:     true,
       objectsInArrays: false,
       arraysInArrays:  false,
     }],
-
-    'brace-style': [ 'error', 'stroustrup', {
-      allowSingleLine: true,
+    /**
+     * FUNCTIONS
+     */
+    'brace-style':                 [ 'error', 'stroustrup', { allowSingleLine: true }],
+    'space-before-function-paren': [ 'error', {
+      anonymous:  'never',
+      named:      'never',
+      asyncArrow: 'always',
     }],
-    'space-before-function-paren': [
-      'error', {
-        anonymous:  'never',
-        named:      'never',
-        asyncArrow: 'always',
+    'no-multiple-empty-lines': [ 'error', {
+      max:    2,
+      maxEOF: 1,
+    }],
+    'no-multi-spaces': [ 'error', {
+      exceptions: {
+        VariableDeclarator:   true,
+        ImportDeclaration:    true,
+        AssignmentExpression: true,
+        ClassProperty:        true,
       },
-    ],
-    'no-multiple-empty-lines': [
-      'error', {
-        max:    2,
-        maxEOF: 1,
+    }],
+    'one-var': [ 'error', {
+      'const':            'never',
+      'var':              'consecutive',
+      let:              'consecutive',
+      separateRequires: true,
+    }],
+    'key-spacing': [ 'error', {
+      singleLine: {
+        beforeColon: false,
+        afterColon:  true,
       },
-    ],
-    'no-multi-spaces': [
-      'error', {
-        exceptions: {
-          VariableDeclarator:   true,
-          ImportDeclaration:    true,
-          AssignmentExpression: true,
-          ClassProperty:        true,
-        },
+      multiLine:  {
+        beforeColon: false,
+        afterColon:  true,
       },
-    ],
-    'one-var': [
-      'error', {
-        const:            'never',
-        var:              'consecutive',
-        let:              'consecutive',
-        separateRequires: true,
+      align: {
+        beforeColon: false,
+        afterColon:  true,
+        mode:        'minimum',
+        on:          'value',
       },
-    ],
-    'key-spacing': [
-      'error', {
-        singleLine: {
-          beforeColon: false,
-          afterColon:  true,
-        },
-        multiLine: {
-          beforeColon: false,
-          afterColon:  true,
-        },
-        align: {
-          beforeColon: false,
-          afterColon:  true,
-          mode:        'strict',
-          on:          'value',
-        },
-      },
-    ],
-    'keyword-spacing': [
-      'error', {
-        before: true,
-        after:  true,
-      },
-    ],
+    }],
+    'keyword-spacing': [ 'error', {
+      before: true,
+      after:  true,
+    }],
     // import
     'import/no-unresolved': [ 'error', {
       commonjs: true,
       amd:      true,
     }],
-    'sort-imports': [ 2, {
+    'sort-imports':         [ 2, {
       ignoreCase:            false,
       ignoreDeclarationSort: true,
       ignoreMemberSort:      true,
       memberSyntaxSortOrder: [ 'single', 'all', 'multiple', 'none' ],
     }],
-    'import/order': [
-      2, {
-        'newlines-between': 'always',
-        groups:             [ 'builtin', 'external', 'internal', 'parent', 'sibling', 'index' ],
-      },
-    ],
+    'import/order': [ 2, {
+      'newlines-between': 'always',
+      groups:             [ 'builtin', 'external', 'internal', 'parent', 'sibling', 'index' ],
+    }],
   },
 };
