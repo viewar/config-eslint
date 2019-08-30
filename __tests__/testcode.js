644f699a87e0cde727f723e03e1dcec6aa73e4ae
SF <sf@viewar.com>
2019-08-30 13:40:56 +0200

    add some more testcode / examples

--- __tests__/testcode.js
+++ __tests__/testcode.js
@@ -1,17 +1,25 @@
-import { failedImportNamed } from '../env/node';
+let failedImport = require('../env/node.js');
+const { failedImportNamed } = require('../env/node');
 
-const failedImport  = require('../env/noddfsfe.js');
-const failedImport2 = require('../env/node.js');
+const myObject = {
+  'var':           true,
+  exp:           'test',
+  dfsfg:         'fdsfgsd',
+  arrayInObject: [ 1, 2, 3 ],
+};
 
-const test = (test) => {
-  if (var1 == 'test') {
+const test = () => {
+  if (var1 === 'test') {
     const test1 = true;
     const test2 = false;
   }
   console.log('failedImportNamed :', failedImportNamed);
+
+  let test = true,
+      test2 = false;
 };
 
-const testObject = {
+let testObject = {
   test: 'fdsfg',
   foo:  'bar',
 };
