ee2657e54b0e5d7760c21d261fa7d8d31ef33c61
SF <sf@viewar.com>
2019-09-23 17:12:15 +0200

    feat(react-rules): add first draft

--- rules/react.js
+++ rules/react.js
@@ -1,5 +1,116 @@
 module.exports = {
   rules: {
     'react/jsx-closing-bracket-location': [ 1, 'line-aligned' ],
+
+    'react/default-props-match-prop-types':       [ 'warn' ],
+    'react/display-name':                         [ 'error' ],
+    'react/no-access-state-in-setstate':          [ 'error' ],
+    'react/no-array-index-key':                   [ 'error' ],
+    'react/no-children-prop':                     [ 'error' ],
+    'react/no-danger':                            [ 'error' ],
+    'react/no-danger-with-children':              [ 'error' ],
+    'react/no-deprecated':                        [ 'error' ],
+    'react/no-direct-mutation-state':             [ 'error' ],
+    'react/no-did-mount-set-state':               [ 'error' ],
+    'react/no-find-dom-node':                     [ 'warn' ],
+    'react/no-is-mounted':                        [ 'error' ],
+    'react/no-redundant-should-component-update': [ 'error' ],
+    'react/no-render-return-value':               [ 'error' ],
+    'react/no-typos':                             [ 'error' ],
+    'react/no-string-refs':                       [ 'error' ],
+    'react/no-this-in-sfc':                       [ 'error' ],
+    'react/no-unescaped-entities':                [ 'error' ],
+    'react/no-unknown-property':                  [ 'error' ],
+    'react/no-unsafe':                            0,
+    'react/no-unused-prop-types':                 [ 'error' ],
+    'react/no-unused-state':                      [ 'error' ],
+    'react/no-will-update-set-state':             [ 'error' ],
+    'react/prefer-read-only-props':               [ 'error' ],
+    'react/prop-types':                           [ 'error' ],
+    'react/require-default-props':                [ 'error' ],
+    'react/require-render-return':                [ 'error' ],
+    'react/self-closing-comp':                    [ 'error' ],
+    'react/sort-comp':                            [ 'error' ],
+    'react/state-in-constructor':                 [ 'error' ],
+    'react/static-property-placement':            [ 'error' ],
+    'react/style-prop-object':                    [ 'error' ],
+    'react/void-dom-elements-no-children':        [ 'error' ],
+    // JSX RULES
+    'react/jsx-boolean-value':                    [ 'error' ],
+    'react/jsx-closing-tag-location':             [ 'error' ],
+    'react/jsx-curly-spacing':                    [ 'error', 'never' ],
+    'react/jsx-equals-spacing':                   [ 'error', 'never' ],
+    'react/jsx-first-prop-new-line':              [ 'error', 'multiline-multiprop' ],
+    'react/jsx-indent':                           [ 'error', 2 ],
+    'react/jsx-indent-props':                     [ 'error', 2 ],
+    'react/jsx-key':                              [ 'error' ],
+    'react/jsx-max-depth':                        [ 'error', { max: 5 }],
+    'react/jsx-no-bind':                          [ 'error' ],
+    'react/jsx-no-comment-textnodes':             [ 'error' ],
+    'react/jsx-no-duplicate-props':               [ 'error' ],
+    'react/jsx-no-target-blank':                  [ 'error' ],
+    'react/jsx-no-undef':                         [ 'error' ],
+    'react/jsx-one-expression-per-line':          0,
+    'react/jsx-curly-brace-presence':             [
+      'error',
+      {
+        props:    'ignore',
+        children: 'ignore',
+      },
+    ],
+    'react/jsx-pascal-case':           [ 'error' ],
+    'react/jsx-props-no-multi-spaces': [ 'error' ],
+    'react/jsx-sort-default-props':    0,
+    'react/jsx-sort-props':            0,
+    'react/jsx-space-before-closing':  [ 'error', 'always' ],
+    'react/jsx-tag-spacing':           [
+      'error',
+      {
+        closingSlash:      'never',
+        beforeSelfClosing: 'always',
+        afterOpening:      'never',
+        beforeClosing:     'never',
+      },
+    ],
+    'react/jsx-uses-react':      [ 'error' ],
+    'react/jsx-uses-vars':       [ 'error' ],
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        declaration:  'parens-new-line',
+        assignment:   'parens-new-line',
+        'return':     'parens-new-line',
+        arrow:        'parens-new-line',
+        condition:    'parens-new-line',
+      },
+    ],
+
+    // unsure - may be changed
+    'react/jsx-props-no-spreading': [ 'warn' ],
+    'react/jsx-filename-extension': [ 'error', { extensions: [ '.jsx' ]}],
+    'react/jsx-curly-newline':      [
+      'error',
+      {
+        multiline:  'consistent',
+        singleline: 'consistent',
+      },
+    ],
+    'react/jsx-max-props-per-line':    0,
+    'react/jsx-handler-names':         [ 'error' ],
+    'react/jsx-child-element-spacing': 0,
+    'react/react-in-jsx-scope':        [ 'error' ], // can be skipped with webpack global module!?
+    'react/prefer-es6-class':          [ 'warn' ],
+    'react/prefer-stateless-function': [ 'error', { ignorePureComponents: true }],
+
+    // coming soon (next version)
+    // "react/jsx-fragments": ["error", "element"],
+    // "react/jsx-no-useless-fragment": ["error"]
+    // DEPRECATED:
+    // Definition for rule 'react/prefer-read-only-props' was not found.eslint(react/prefer-read-only-props)
+    // Definition for rule 'react/state-in-constructor' was not found.eslint(react/state-in-constructor)
+    // Definition for rule 'react/static-property-placement' was not found.eslint(react/static-property-placement)
+    // Definition for rule 'react/jsx-props-no-multi-spaces' was not found.eslint(react/jsx-props-no-multi-spaces)
+    // Definition for rule 'react/jsx-props-no-spreading' was not found.eslint(react/jsx-props-no-spreading)
+    // Definition for rule 'react/jsx-curly-newline' was not found.eslint(react/jsx-curly-newline)
   },
 };
