381253975cbabfadb785251e10ee4d3620d0ae3c
viewar_sf <dev@project23.org>
2019-10-15 18:32:53 +0200

    docs(README): clean up

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  printWidth: 90,
-  tabWidth: 2,
-  useTabs: false,
-  singleQuote: true,
-  trailingComma: 'es5',
-  arrowParens: 'always',
-  semi: true,
+  printWidth:     90,
+  tabWidth:       2,
+  useTabs:        false,
+  singleQuote:    true,
+  trailingComma:  'es5',
+  arrowParens:    'always',
+  semi:           true,
   bracketSpacing: true,
-  proseWrap: 'preserve',
-  quoteProps: 'as-needed',
-  overrides: [
+  proseWrap:      'preserve',
+  quoteProps:     'as-needed',
+  overrides:      [
     {
-      files: '*.{js,jsx}',
+      files:   '*.{js,jsx}',
       options: {
         parser: 'babel',
       },
