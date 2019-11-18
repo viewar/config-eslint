489ae441013a28ab42eeaf529013369c1ba449ee
SF <sf@viewar.com>
2019-11-18 15:26:53 +0100

    format

--- utils/getResolveConfigPath.js
+++ utils/getResolveConfigPath.js
@@ -12,12 +12,12 @@ const getResolveConfigPath = (pathToConfigs) => {
     resolvedPath = path.resolve(
       process.cwd(),
       pathToConfigs || process.env.CONFIG_PATH || '',
-      './webpack.config.resolve.js'
+      './webpack.config.resolve.js',
     );
     process.env.DEBUG &&
       console.log(
         '\n[@viewar/webpack] Info: try to load resolve config from:\n',
-        resolvedPath
+        resolvedPath,
       );
     // verify if file is loadable
     require(resolvedPath);
@@ -26,7 +26,7 @@ const getResolveConfigPath = (pathToConfigs) => {
     if (e.code === 'MODULE_NOT_FOUND') {
       process.env.DEBUG &&
         console.log(
-          '\n[@viewar/webpack] Warning: webpack.config.resolve.js not found - using \'eslint-config-viewar/utils/webpack.config.resolve.js\'\n'
+          '\n[@viewar/webpack] Warning: webpack.config.resolve.js not found - using \'eslint-config-viewar/utils/webpack.config.resolve.js\'\n',
         );
     }
     else {
@@ -41,7 +41,7 @@ const getResolveConfigPath = (pathToConfigs) => {
     console.log(
       '\n[@viewar/webpack] Info: resolve config found under:\n',
       resolvedPath,
-      '\n'
+      '\n',
     );
 
   return resolvedPath;
